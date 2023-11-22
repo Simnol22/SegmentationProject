c95f182801a523d3964d2e2b623b39f49e47fb4e
Simon <simonkirbi@hotmail.com>
2023-11-21 20:47:14 -0500

    Very basic nnUnet integration

--- code/nnUnet.py
+++ code/nnUnet.py
@@ -1,15 +1,27 @@
 from nnunet.network_architecture.generic_UNet import Generic_UNet
 
-def nnUnet(num_classes):
-    from nnunet.network_architecture.generic_UNet import Generic_UNet
-    model = Generic_UNet(input_channels=1, base_num_features=64, num_classes=num_classes,num_pool=1)
 
-    for name, parameter in model.named_parameters():
-        if 'seg_outputs' in name:
-            print(f"parameter '{name}' will not be freezed")
-            parameter.requires_grad = True
-        else:
-            parameter.requires_grad = False
+class nnUnet(Generic_UNet):
+    def __init__(self, num_classes):
+        super().__init__(input_channels=1, base_num_features=64, num_classes=num_classes,num_pool=1)
 
+    def parameters(self):
+        return super().parameters()
+    
+    def forward(self, x):
+        x = super().forward(x)[0]
+        return x
 
-    return model
+#def nnUnet(num_classes):
+#    from nnunet.network_architecture.generic_UNet import Generic_UNet
+#    model = Generic_UNet(input_channels=1, base_num_features=64, num_classes=num_classes,num_pool=1)
+#
+#    for name, parameter in model.named_parameters():
+#        if 'seg_outputs' in name:
+#            print(f"parameter '{name}' will not be freezed")
+#            parameter.requires_grad = True
+#        else:
+#            parameter.requires_grad = False
+#
+#
+#    return model
